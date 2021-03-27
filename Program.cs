76f23e708eb9945ae1bfbdc1dd5a8bab7f1360c9
NicolasOsmundo <nicolasosmundo@hotmail.com>
2021-03-26 23:23:25 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class Program
+{
+	static void Main(string[] args)
+	{
+		try
+		{
+			string nome = null;
+			Console.WriteLine(nome.Substring(2));
+			new Curso().CriarAluno(nome);
+		}
+		// Capturando uma exceção de referência nula.
+		// Similar ao NullPointerExceptio do  Java
+		catch (NullReferenceException)
+		{
+			Console.WriteLine("Nome do aluno incorreto");
+		}
+		catch (Exception)
+		{
+			Console.WriteLine("Problema na execução a operação");
+			throw new Exception("Problema na execução a operação");
+		}
+	}
+}
